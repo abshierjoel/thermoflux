0d6b554c4750349a53cd0541b71212b03d4fa26d
Joel Abshier <jabshier@influxdata.com>
2022-01-07 09:52:49 -0600

    🤐 Remove mock data

--- lib/thermoflux/ecobee.ex
+++ lib/thermoflux/ecobee.ex
@@ -7,13 +7,6 @@ defmodule Thermoflux.Ecobee do
   plug(Tesla.Middleware.JSON)
 
   @api_key Application.get_env(:thermoflux, :ecobee_api_key)
-  # @access_token %{
-  #   "access_token" => "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJFWXhNVEpDT0Rnek9UaERRelJHTkRCRlFqZEdNVGxETnpaR1JUZzRNalEwTmtWR01UQkdPQSJ9.eyJpc3MiOiJodHRwczovL2F1dGguZWNvYmVlLmNvbS8iLCJzdWIiOiJhdXRoMHwxNDVmMzEwZS1jYjY4LTQ5MGEtOTdmOS1iMjU2MzIwODgyMmYiLCJhdWQiOlsiaHR0cHM6Ly9kZXZlbG9wZXItYXBwcy5lY29iZWUuY29tL2FwaS92MSIsImh0dHBzOi8vZWNvYmVlLXByb2QuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY0MTQyMjc3NSwiZXhwIjoxNjQxNDI2Mzc1LCJhenAiOiJ4NVkyeHBRYUpvRmpLQUFCd0RTMW9JcXhUcEhZOTR1WiIsInNjb3BlIjoib3BlbmlkIHNtYXJ0V3JpdGUgb2ZmbGluZV9hY2Nlc3MifQ.UJwvMxATW4nTW3cPZIubACRDzmR9e6-VGN5e3cb0d5shMdnNo8lzcbKorJkKTIPiDphO_LbDXHcFdP3gP2shHqZVwr3zXGfZFwgZYN4O8Ggutoeb17HIzdlH8OATet-Dh39vUi2s-3q1e4QgxNIL6JGyr7LookFX4HyTDA5NQFC_kI86TBnn33fh_Nk_kf3RskGAWvq6qtCGTVEo2mKRvl8LGqf3c9JpuE4gSWMdr5TeG0T4esIE03Cn3J2oBNt8TNRT-O48N3-QO97dRt_m6AAlBaGkH3WTclm6QO-ikXcV1yQUHu02v5uCcW6SETJO9ptg8l0bMrqmLKeUJp5vWQ",
-  #   "expires_in" => 3600,
-  #   "refresh_token" => "knaJ6QnaJaP06idHRWPFWfZ4y-oyPCle3tT3KMlwOtoMN",
-  #   "scope" => "openid,smartWrite,offline_access",
-  #   "token_type" => "Bearer"
-  # }
 
   def authorization do
     query = [response_type: "ecobeePin", client_id: @api_key, scope: "smartWrite"]
